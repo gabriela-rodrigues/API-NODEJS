8830faeff7369ccad6e2d6a936123cd5855385d2
gabriela-rodrigues <gabrielarodrigues502@hotmail.com>
2022-08-17 12:48:45 -0300

    first commit

---
+++ config/routes.js
@@ -0,0 +1,36 @@
+const express = require ('express')
+const routes = express.Router()
+
+let db =[
+    {'1':{nome : 'cliente 1', idade :'20'}},
+    {'2':{nome: 'cliente 2', idade :'21'}},
+    {'3':{nome: 'ciente 3', idade :'25'}}
+]
+
+ 
+routes.get('/', (req, res) =>{
+    return res.json(db)
+})
+
+
+routes.post( '/add', (req, res) => {
+const body= req.body
+
+    if(!body)
+    return res.status(400).end()
+
+    db.push(body)
+    return res.json(body)
+})
+
+routes.delete('/:id', (req, res) =>{
+    const id = req.params.id
+    let newDB = db.filter(item => {
+    if(!item[id])
+    return item
+})
+    db = newDB
+    return res.send(newDB)
+})
+
+module.exports = routes
